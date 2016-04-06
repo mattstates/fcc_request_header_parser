183b4c85840dc840616005c152aede25ce0c7c63
Matt States <mattstates@icloud.com>
2016-04-05 20:02:11 -0700

    ready for heroku deployment

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send('This is a test');
+        //console.log(req.headers);
+        res.send({
+            'ipaddress': req.ip,
+            'language': req.headers['accept-language'].split(',')[0],
+            'software': req.headers['user-agent'].split('(')[1].split(')')[0]
+        });
 });
 
-app.listen(port);
+app.listen(port, function() {
+    console.log('This server is listening on port: ' + port);
+});
